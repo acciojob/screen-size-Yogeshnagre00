96f0b63cfa6bc4558afa955147be58027ce67e40
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-05 15:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-//your JS code here. If required.
-function getSize(){
-	 const w = document.documentElement.clientWidth;
-     const h = document.documentElement.clientHeight;
-
-	const elem = document.getElementById('sizeInfo');
-	elem.innerHTML="<h1> Width: "+w+" and Height: "+h+"</h1";
-}
+const app = require("./index.js");
+app.listen(3000, () => {
+  console.log('server started');
+});
