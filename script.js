700c6d29c7c24493c6229df501d8aa8701c1c28d
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-07-05 15:20:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const app = require("./index.js");
-app.listen(3000, () => {
-  console.log('server started');
-});
+//your JS code here. If required.
+  let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+    h.innerText="Width: "+width+" and Height: "+height;  
+window.addEventListener("resize",function(){
+    let height=this.innerHeight;
+    let width=this.innerWidth;
+   let div=document.getElementById("sizeInfo");
+    let h=div.firstChild;
+
+    h.innerText="Width: "+width+" and Height: "+height; 
+  
+  })
